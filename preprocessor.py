6e6ba3614db096bb33940000132201f2cdc0db28
SnkhchyanV <82439632+SnkhchyanV@users.noreply.github.com>
2023-04-22 00:50:38 +0400

    No comment

--- preprocessor.py
+++ preprocessor.py
@@ -7,46 +7,45 @@ from sklearn.feature_selection import SelectKBest, f_classif
 
 class Preprocessor:
     def __init__(self):
-        self.NaN_new_values = None
-        self.Parameters_for_column_selection = None
-        self.Scaler_parameter = None
+        self.imp = None
+        self.sel = None
+        self.scaler = None
     def fit(self, data):
-        ## Here is a just fit_transfrom
+        # By the way Here is a just fit-transformer
         target = 'In-hospital_death'
         X = data.drop([target], axis=1)
         y = data[target]
+
+        # Replacing all NaN with medians of column, and then saving the imputer to call transform in the future
         imp = SimpleImputer(missing_values=np.nan, strategy='median')
         imp.fit(X)
         X_train = imp.transform(X)
-        self.NaN_new_values = imp.get_params()
+        self.imp = imp
 
+        # Fixing class imbalance
         smote = SMOTE()  # deals with class imbalance
         X_train, y_train = smote.fit_resample(X_train, y)
 
-        sel = SelectKBest(f_classif, k=10)
+        # Leave the most (top 30) important columns, saving model of selector
+        sel = SelectKBest(f_classif, k=30)
         sel.fit(X_train, y_train)
-        self.Parameters_for_column_selection = sel.get_params()
         X_train = sel.transform(X_train)
+        self.sel = sel
 
+        # Making a scale, and save the scaler
         scaler = StandardScaler()
         scaler.fit(X_train)
-        self.Scaler_parameter = scaler.get_params()
         X_train = scaler.transform(X_train)
+        self.scaler = scaler
 
+        # Return X_train and y_train in case the reusability
         return X_train, y_train
     def transform(self, X):
-        imp = SimpleImputer(missing_values=np.nan, strategy='median')
-        imp.set_params(self.NaN_new_values)
-        X_test = imp.transform(X)
 
-        sel = SelectKBest(f_classif, k=10)
-        sel.set_params(self.Parameters_for_column_selection)
-        X_test = sel.transform(X_test)
-
-        scaler = StandardScaler()
-        scaler.set_params(self.Scaler_parameter)
-        X_test = scaler.transform(X_test)
-
+        # Preprocessing the test data, with the saved models
+        X_test = self.imp.transform(X)
+        X_test = self.sel.transform(X_test)
+        X_test = self.scaler.transform(X_test)
         return X_test
 
 
--- run_pipeline.py
+++ run_pipeline.py
@@ -6,9 +6,10 @@ import json
 import pandas as pd
 import numpy as np
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path", type=str, required=True)
-parser.add_argument("--inference",type=str, default=None)
+parser.add_argument("--inference", type=str, default=None)
 args = parser.parse_args()
 
 class Pipeline:
@@ -18,36 +19,49 @@ class Pipeline:
 
     def Run(self, data_path, inference):
         data = pd.read_csv(data_path)
+
         # Choosing the mode
         if inference is None:
-            self._Run_Train(data)
+            result = self._Run_Train(data)  # training on the data, returns X_train, y_train, y_pred
         else:
-            self._Run_Test(data)
+            result = self._Run_Test(data) # testing the data, returns X_test, y_pred
+
+        # returning the values of functions that are train or test datas, and predictions, in case of after use
+        return result
 
     def _Run_Test(self, data):
+        # loading the saved preprocessor
+        with open('preprocessor.pkl', 'rb') as preprocessor_file:
+            self.preprocessor = pickle.load(preprocessor_file)
+
+        # preprocessing the test data
         X_test = self.preprocessor.transform(data)  # Preprocessing the data, before test
 
         # Loading the trained model
         with open('model.pkl', 'rb') as model_file:
             self.model = pickle.load(model_file)
 
-        # Making the prediction
-        threshold = 0.5
+        # Setting the threshold and gain the probas
+        # threshold is set low because we want to have more false negative (just in case to doctors be more attentive)
+        threshold = 0.3
         y_pred_proba = self.model.predict_proba(X_test)
+
         y_pred = np.zeros(len(y_pred_proba), dtype=int)
-        # use a loop to iterate over each probability value
+        # Make predictions with set threshold
         for i in range(len(y_pred)):
-            if y_pred_proba[i] >= threshold:
-                y_pred[i] = 1
-            else:
+            if y_pred_proba[i][0] >= threshold:
                 y_pred[i] = 0
+            else:
+                y_pred[i] = 1
 
         # Saving outputs in predictions.json file
-        output = {'predict_probes': list(y_pred_proba), 'threshold': threshold}
+        output = {'predict_probes': y_pred_proba.tolist(), 'threshold': threshold}
         with open('predictions.json', 'w') as predictions_file:
             json.dump(output, predictions_file)
 
-        return y_pred
+        # Returning test data, and predicted values in case of the future use
+        # None is here to have the same result such as in the train function
+        return X_test, None, y_pred
 
     def _Run_Train(self, data):
         # Preprocessing
@@ -62,16 +76,36 @@ class Pipeline:
         with open('preprocessor.pkl', 'wb') as preprocessor_file:
             pickle.dump(self.preprocessor, preprocessor_file)
 
-        # Predictions
+        # Predictions, just in case to use for threshold decision
         y_pred = self.model.predict(X_train)
-        # Accuracy
-        accuracy = np.mean(y_train == y_pred)
-        print('Accuracy: ', accuracy)
 
-        return y_pred
+        # Returning test data, and predicted values in case of the future use
+        return X_train, y_train, y_pred
 
 preprocessor_obj = preprocessor.Preprocessor()
 model_obj = model.Model()
 
 pipeline = Pipeline(preprocessor_obj, model_obj)
-pipeline.Run(args.data_path, args.inference)
+X, y_real, y_pred = pipeline.Run(args.data_path, args.inference) #y_real could be None if it was testing
+
+
+### to_delete, just testing
+'''
+data = pd.read_csv("C:/Users/vsnkh/Downloads/hospital_deaths_train.csv")
+
+target = 'In-hospital_death'
+X = data.drop([target], axis=1)
+y = data[target]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=34)
+X_train[target]=y_train
+
+X_train, y_train, y_pred_train = pipeline._Run_Train(X_train) #
+X_test, y_not_real_just_Nones, y_pred_test = pipeline._Run_Test(X_test) #y_test would be None because it was testing
+
+accuracy_train = accuracy_score(y_train, y_pred_train)
+accuracy_test = accuracy_score(y_test, y_pred_test)
+
+print(accuracy_train)
+print(accuracy_test)
+'''
